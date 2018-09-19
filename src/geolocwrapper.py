5dfdd494b3c7de4f72e766d242e60b33b044c092
avaldez22 <adrian.valdez203@yahoo.com>
2018-09-19 14:39:35 -0600

    finished

--- src/geolocwrapper.py
+++ src/geolocwrapper.py
@@ -1,45 +1,29 @@
 # geolocwrapper.py
 
-from geopy.distance import distance
+from geopy.distance import distance, lonlat
 from geopy.geocoders import Nominatim
 from geopy.exc import GeopyError
 
 
 class GeoLocWrapper:
-    """A class that wraps a limited set of utilities around a geopy Location object. Class provides an API for handling operations on a specific geopy.location.Location object.
-
-    https://geopy.readthedocs.io/en/stable/    
-    """
 
     def __init__(self, addr_str=''):
-        """Create a new GeoLocWrapper object that creates a connection to 
-        Nominatum geo web service and initializes a geopy location object from
-        an address string.
-
-        Consider: What happens if the geolocator cannot locate geographic 
-        location from addr_str? Raise a GeopyError exception!
-        """
         self.geolocator = Nominatim(user_agent="test-application")     
         self.location = self.geolocator.geocode(addr_str)
-        
-    def get_distance_miles(self, dest_addr_str=None):
-        """Returns the geodisic distance in miles from location and 
-        dest_addr_str.
-        
-        Consider: What happens if the geolocator cannot locate geographic 
-        location from dest_addr_str?
-        """
-        pass
+        if self.location == None:
+            raise GeopyError
+        self.pos = (self.location.longitude, self.location.latitude)
+    def get_distance_miles(self, pos0, pos1):
+        self.one = (pos0.longitude, pos0.latitude)
+        self.two = (pos1.longitude, pos1.latitude)
+        self.dist_mile = (distance(lonlat(*self.one), lonlat(*self.two)).miles)
+        return self.dist_mile
 
-    def get_distance_kilometers(self, dest_addr_str=None):
-        """Returns the geodisic distance in kilometers from location and 
-        dest_addr_str.
-        
-        Consider: What happens if the geolocator cannot locate geographic 
-        location from dest_addr_str?
-        """
-        pass
-
+    def get_distance_kilometers(self, pos0, pos1):
+        self.one = (pos0.longitude, pos0.latitude)
+        self.two = (pos1.longitude, pos1.latitude)
+        self.dist_kilo = (distance(lonlat(*self.one), lonlat(*self.two)))
+        return (str(self.dist_kilo).split(" "))[0]
 
 
 
--- src/test_geolocwrapper.py
+++ src/test_geolocwrapper.py
@@ -2,6 +2,7 @@
 import unittest
 
 from geolocwrapper import GeoLocWrapper
+from geopy.exc import GeopyError
 
 class TestGeoLocWrapper(unittest.TestCase):
     """A collection of unit tests for the BabySet class. 
@@ -10,9 +11,25 @@ class TestGeoLocWrapper(unittest.TestCase):
     """
     def test_init(self):
         """Unit test for GeoLocWrapper constructor."""
-        pass  # remove this and write the test for the constructor.
-
-    # Begin adding your unit tests for the GeoLocWrapper module.
-
+        gset = GeoLocWrapper("806 National Ave, Las Vegas, NM 87701")
+        self.assertEqual(str(gset.location), "Lora Shields Building - NMHU, 806, National Avenue, Las Vegas, San Miguel County, New Mexico, 87701, USA")
+    def test_init_fail(self):
+        """Unit test for GeoLocWrapper constructor."""
+        with self.assertRaises(GeopyError):
+            gset = GeoLocWrapper("8wfjsagbjhfhsajkat")
+    def test_miles(self):
+        gset = GeoLocWrapper("806 National Ave, Las Vegas, NM 87701")
+        pos0 = gset.location
+        gset = GeoLocWrapper("215 Grand Ave, Las Vegas, NM 87701")
+        pos1 = gset.location
+        gset1 = gset.get_distance_miles(pos0, pos1)
+        self.assertEqual(gset1,0.4582092427954895)
+    def test_kilometers(self):
+        gset = GeoLocWrapper("806 National Ave, Las Vegas, NM 87701")
+        pos0 = gset.location
+        gset = GeoLocWrapper("215 Grand Ave, Las Vegas, NM 87701")
+        pos1 = gset.location
+        gset1 = gset.get_distance_kilometers(pos0, pos1)
+        self.assertEqual(gset1,"0.737416295637")
 if __name__ == '__main__':
     unittest.main()
