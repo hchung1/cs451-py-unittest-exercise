ca42edafa97e5476a0458969c75d5cd97228002d
avaldez22 <adrian.valdez203@yahoo.com>
2018-09-19 14:58:55 -0600

    Cleaned up code

--- src/geolocwrapper.py
+++ src/geolocwrapper.py
@@ -5,14 +5,13 @@ from geopy.geocoders import Nominatim
 from geopy.exc import GeopyError
 
 
-class GeoLocWrapper:
-
-    def __init__(self, addr_str=''):
+class GeoLocWrapper(object):
+    def locating(self, addr_str=''):
         self.geolocator = Nominatim(user_agent="test-application")     
         self.location = self.geolocator.geocode(addr_str)
         if self.location == None:
             raise GeopyError
-        self.pos = (self.location.longitude, self.location.latitude)
+        return self.location
     def get_distance_miles(self, pos0, pos1):
         self.one = (pos0.longitude, pos0.latitude)
         self.two = (pos1.longitude, pos1.latitude)
--- src/test_geolocwrapper.py
+++ src/test_geolocwrapper.py
@@ -5,31 +5,25 @@ from geolocwrapper import GeoLocWrapper
 from geopy.exc import GeopyError
 
 class TestGeoLocWrapper(unittest.TestCase):
-    """A collection of unit tests for the BabySet class. 
-    Used to demonstrate and introduce unit testing in Python.
-    Related docs: https://docs.python.org/3/library/unittest.html
-    """
     def test_init(self):
-        """Unit test for GeoLocWrapper constructor."""
-        gset = GeoLocWrapper("806 National Ave, Las Vegas, NM 87701")
-        self.assertEqual(str(gset.location), "Lora Shields Building - NMHU, 806, National Avenue, Las Vegas, San Miguel County, New Mexico, 87701, USA")
+        a = GeoLocWrapper()
+        gset = a.locating("806 National Ave, Las Vegas, NM 87701")
+        self.assertEqual(str(gset), "Lora Shields Building - NMHU, 806, National Avenue, Las Vegas, San Miguel County, New Mexico, 87701, USA")
     def test_init_fail(self):
-        """Unit test for GeoLocWrapper constructor."""
+        a = GeoLocWrapper()
         with self.assertRaises(GeopyError):
-            gset = GeoLocWrapper("8wfjsagbjhfhsajkat")
+            gset = a.locating("8wfjsagbjhfhsajkat")
     def test_miles(self):
-        gset = GeoLocWrapper("806 National Ave, Las Vegas, NM 87701")
-        pos0 = gset.location
-        gset = GeoLocWrapper("215 Grand Ave, Las Vegas, NM 87701")
-        pos1 = gset.location
-        gset1 = gset.get_distance_miles(pos0, pos1)
-        self.assertEqual(gset1,0.4582092427954895)
+        a = GeoLocWrapper()
+        pos1 = a.locating("806 National Ave, Las Vegas, NM 87701")
+        pos2 = a.locating("215 Grand Ave, Las Vegas, NM 87701")
+        gset = a.get_distance_miles(pos1, pos2)
+        self.assertEqual(gset,0.4582092427954895)
     def test_kilometers(self):
-        gset = GeoLocWrapper("806 National Ave, Las Vegas, NM 87701")
-        pos0 = gset.location
-        gset = GeoLocWrapper("215 Grand Ave, Las Vegas, NM 87701")
-        pos1 = gset.location
-        gset1 = gset.get_distance_kilometers(pos0, pos1)
-        self.assertEqual(gset1,"0.737416295637")
+        a = GeoLocWrapper()
+        pos1 = a.locating("806 National Ave, Las Vegas, NM 87701")
+        pos2 = a.locating("215 Grand Ave, Las Vegas, NM 87701")
+        gset = a.get_distance_kilometers(pos1, pos2)
+        self.assertEqual(gset,"0.737416295637")
 if __name__ == '__main__':
     unittest.main()
